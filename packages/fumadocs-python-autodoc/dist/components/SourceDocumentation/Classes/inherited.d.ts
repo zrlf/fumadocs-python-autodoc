92da7f5ced3fea95fb6ee5cf33195785df97f408
florez <florez@ethz.ch>
2025-10-03 15:43:28 +0200

    fix(classes): split inherited bases into server and client such that linking works correctly (only on server possible)

--- packages/fumadocs-python-autodoc/dist/components/SourceDocumentation/Classes/inherited.d.ts
+++ packages/fumadocs-python-autodoc/dist/components/SourceDocumentation/Classes/inherited.d.ts
@@ -1,5 +1,10 @@
+import { ComponentProps } from "react";
 import { ClassInterface } from "../types";
 export declare const Bases: ({ data, }: {
     data: ClassInterface["inherited_members"];
 }) => import("react/jsx-runtime").JSX.Element;
+export declare function Base({ cls, members, className, ...props }: {
+    cls: string;
+    members: ClassInterface["inherited_members"][string];
+} & ComponentProps<"div">): import("react/jsx-runtime").JSX.Element;
 //# sourceMappingURL=inherited.d.ts.map
